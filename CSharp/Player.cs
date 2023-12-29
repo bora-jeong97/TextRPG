27716a5531aa3994f3f9c3db5de0a1e5a8ce3eed
bora-jeong97 <purple7714@naver.com>
2023-12-30 06:42:34 +0900

    TextRPG 몬스터 생성
    OOP방식으로 구성. 상속을 통한 구현 및 전투

---
+++ CSharp/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+
+    class Player : Creature
+    {
+        protected PlayerType type = PlayerType.None;
+
+        protected Player(PlayerType type) : base(CreatureType.Player) // 매개변수 필수. 기본 생성자는 안만들었기 때문에 무조건 type을 넣어야 사용가능하다.
+        {
+            this.type = type;
+        }
+
+        public PlayerType GetPlayerType() { return type; }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Player player = new Knight();
+            Player player2 = new Archer();
+            Monster monster = new Orc();
+
+            // 전투 : 플레이어 vs 몬스터 
+            int damage = player.GetAttack();    // 플레이어 공격력
+            monster.OnDamaged(damage);  // 몬스터 타격
+
+            // PVP
+            player2.OnDamaged(damage);
+        }
+    }
+}
